59c7bd64de2d237461d6c974023f636c130e9983
hustcc <i@atool.org>
2016-05-31 12:56:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,26 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+LONGDOC = """
+A very simple python lib, used to format datetime with *** time ago statement.
+
+Install
+
+    pip install timeago
+
+Usage
+
+import timeago, datetime
+d = datetime.datetime.now() + datetime.timedelta(seconds = 60 * 3.4)
+# locale
+print (timeago.format(d, locale='zh_CN')) # will print 3分钟后
+
+"""
+
 setup(name = 'timeago',
       version = '1.0.3',
       description = 'A very simple python library, used to format datetime with `*** time ago` statement. eg: "3 hours ago".',
-      long_description = '',
+      long_description = LONGDOC,
       author = 'hustcc',
       author_email = 'i@hust.cc',
       url = 'https://github.com/hustcc/timeago',
