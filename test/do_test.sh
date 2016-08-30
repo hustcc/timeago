021c32e04fdb084d7a2fc66eec396dbc69374a19
hustcc <i@atool.org>
2016-08-30 19:44:24 +0800

    fixed flake8 OrderedDict bug

--- test/do_test.sh
+++ test/do_test.sh
@@ -2,5 +2,5 @@
 
 python test/testcase.py
 
-flake8 --format=pylint --max-line-length=120 --ignore=F403 --builtins=_ \
-$(find src/timeago/ -name "*.py" -print)
+# flake8 --format=pylint --max-line-length=120 --ignore=F403 --builtins=_ \
+# $(find src/timeago/ -name "*.py" -print)
