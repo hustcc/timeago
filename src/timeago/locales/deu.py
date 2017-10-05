99c8a21a836e5d262c82a16c51c788db7e52c0e8
thinhlx1993 <thinhlx1993@gmail.com>
2017-10-05 23:50:59 +0700

    Create deu.py

---
+++ src/timeago/locales/deu.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2016-5-26
+@author: thinhlx
+'''
+
+LOCALE = [
+     ["gerade jetzt", "eine weile"],
+     ["% s Sekunden zuvor", "im% s Sekunden"],
+     ["Vor 1 Minute", "in 1 Minute"],
+     ["% s Vor ein paar Minuten", "im% s Minute"],
+     ["1 vor einer Stunde", "im 1 Stunde"],
+     ["% s vor einer Stunde", "in% s Stunden"],
+     ["Vor 1 Tag", "in 1 Tag"],
+     ["% s Tage vor", "in% s Tage"],
+     ["Vor 1 Woche", "in 1 Woche"],
+     ["% s Wochen vor", "in% s Wochen"],
+     ["Vor 1 Monat", "in 1 Monat"],
+     ["% s Monate vor", "in% s Monate"],
+     ["Vor 1 Jahr", "in 1 Jahr"],
+     ["% s Jahre vor", "in% s Jahre"],
+]
