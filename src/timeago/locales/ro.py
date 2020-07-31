9ba89a2c742b04cb908dcf1ae1284bde2f52ee50
abel <radac@crowdbotics.com>
2020-07-31 21:26:36 +0300

    add ro translation

---
+++ src/timeago/locales/ro.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2020-07-31
+
+@author: abelradac
+'''
+
+LOCALE = [
+    ["chiar acum", "în curând"],
+    ["acum %s secunde", "în %s secunde"],
+    ["acum 1 minut", "într-un minut"],
+    ["acum %s minute", "în %s minute"],
+    ["acum 1 oră", "într-o oră"],
+    ["acum %s ore", "în %s ore"],
+    ["acum 1 zi", "într-o zi"],
+    ["acum %s zile", "în %s days"],
+    ["acum 1 săptămână", "într-o săptămână"],
+    ["acum %s săptămâni", "în %s săptămâni"],
+    ["acum 1 lună", "într-o lună"],
+    ["acum %s luni", "în %s luni"],
+    ["acum 1 an", "într-un an"],
+    ["acum %s ani", "în %s ani"],
+]
